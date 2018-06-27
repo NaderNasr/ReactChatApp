92194212808ff53f0f95084a62b789778c477e61
Nader Nasr <nadernasr7@gmail.com>
2018-06-28 02:44:11 +0300

    Add router flux

---
+++ src/components/Chat.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import {
+
+  View,
+  Text,
+
+} from 'react-native'
+
+class Chat extends React.Component{
+  render(){
+    return(
+      <View>
+        <Text>
+          Hello Home
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default Chat;
