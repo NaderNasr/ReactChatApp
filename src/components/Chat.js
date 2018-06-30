cbd08633e48b94198e692f5664c22a322b90fb9f
Nader Nasr <nadernasr7@gmail.com>
2018-06-30 14:14:00 +0300

    finalize chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,7 +4,7 @@ import React from 'react'
 import {
 
   GiftedChat
-  
+
 } from 'react-native-gifted-chat'
 
 class Chat extends React.Component{
@@ -15,7 +15,10 @@ class Chat extends React.Component{
     return(
       <GiftedChat
         messages={this.state.messages}
-        onSend={messages => this.onSend(messages)}
+        onSend={(messages) => {
+          //this.onSend(messages)
+          //send to backend
+        }}
         user={{
           _id: 1,
         }}
